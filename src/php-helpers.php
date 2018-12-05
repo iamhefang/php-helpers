64e4731c65f995fd55506ccdda9b50f0640d6ec4
hefang <he@hefang.link>
2018-12-05 11:52:08 +0800

    init2

--- src/php-helpers.php
+++ src/php-helpers.php
@@ -1,8 +1,7 @@
 <?php
-
 define("PHP_HELPERS", true);
 define("PHP_HELPERS_VERSION", "2018.12.05");
 
-require __DIR__ . DIRECTORY_SEPARATOR . "link" . DIRECTORY_SEPARATOR . "hefang" . DIRECTORY_SEPARATOR . "helpers" . DIRECTORY_SEPARATOR . "ClassHelper.class.php";
+require __DIR__ . "/link/hefang/helpers/ClassHelper.class.php";
 
 \link\hefang\helpers\ClassHelper::loader(__DIR__);
